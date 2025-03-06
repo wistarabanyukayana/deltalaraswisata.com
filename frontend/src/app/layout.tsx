848e5ee7430a77dded2ae151b84e267f2c9ef2b9
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-03-06 20:17:43 +0700

    favicons organized

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,73 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Delta Laras Wisata",
   description: "Travel Handal dan Terpecaya",
+  icons: {
+    apple: [
+      { url: "apple-icon.png?v=1" },
+      {
+        url: "apple-icon-57x57.png?v=1",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-60x60.png?v=1",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-72x72.png?v=1",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-76x76.png?v=1",
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-114x114.png?v=1",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-120x120.png?v=1",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-144x144.png?v=1",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-152x152.png?v=1",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: "apple-icon-180x180.png?v=1",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    icon: [
+      {
+        url: "android-icon-192x192.png?v=1",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      { url: "favicon-16x16.png?v=1", sizes: "16x16", type: "image/png" },
+      { url: "favicon-32x32.png?v=1", sizes: "32x32", type: "image/png" },
+      { url: "favicon-96x96.png?v=1", sizes: "96x96", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "apple-icon-precomposed",
+        url: "apple-icon-precomposed.png?v=1",
+      },
+    ],
+  },
+  manifest: "manifest.json",
 };
 
 export default function RootLayout({
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -30,13 +20,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             Deploy now
           </a>
           <a
@@ -56,13 +39,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
           Learn
         </a>
         <a
@@ -71,13 +47,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
           Examples
         </a>
         <a
@@ -86,13 +55,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
           Go to nextjs.org →
         </a>
       </footer>
